d40c03fc7a68b378945452441de473d0b5030cee
monzuralam16 <monzur.alam.16@gmail.com>
2023-03-27 23:30:31 +0600

    navbar done+hero section on process

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { styles } from '../styles'
+import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    // h-screen sets the height of an element to fill the entire height of the screen.  used in combination with other layout and positioning classes to create full-height sections or containers
+      <section className='relative w-full h-screen mx-auto '>
+        {/* inset-0 sets the top, right, bottom, and left properties of an element to 0, effectively positioning the element at the edges of its container. */}
+            <div className={`${styles.paddingX} absolute flex flex-row  top-[120px] inset-0 max-w-7xl items-start gap-5`}>
+              <div className='flex flex-col justify-center items-center'>
+                <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
+                <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+              </div>
+              <div>
+                <h1 className={`${styles.heroHeadText}`}>Hello World! I'm <span className='text-[#915eff]'>Manzur Alam</span> </h1>
+                <p className={`${styles.heroSubText} mt-2 text-white-100`}> I develop Web Application using MERN</p>
+              </div>
+            </div>
+            <ComputersCanvas/>
+      </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,15 @@ import {logo,menu,close} from "../assets";
 
 
 const Navbar = () => {
+  // to keep track of active tab  
   const [active,setActive] = useState('');
+  const [toggle,setToggle] = useState(false)
+
 
   return (
     <nav className ={ `${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      {/* using logo and name as a link to homepage and scroll to 0,0 position  */}
+      {/* items-center place the children horizontally center */}
      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
           to="/"
@@ -23,18 +28,60 @@ const Navbar = () => {
           }
         >
           <img src={logo} alt="logo" className='w-9 h-9 object-contain'></img>
-          <p className='text-white text-[-18px] font-bold cursor-pointer'>Manzur <span className='sm:block hidden'>| Portfolio</span></p>
+          <p className='text-white text-[-18px] font-bold cursor-pointer flex'>Manzur Alam &nbsp; <span className='sm:block hidden'>| Front End Developer</span></p>
         </Link>
-        {/* <ul className='list-none hidden sm:flex flex-row gap-10'>
+
+        {/* <h1>{navLinks.length}</h1> */}
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
             {
-              navLinks.map(link =>{
-                <li key={link.id}>
+              navLinks.map((link) => 
+                 <li 
+                  key={link.id}
+                  className={
+                  `${
+                    active === link.title? "text-white" : "text-secondary"
+                  } hover:text-white text-[18px] font-medium cursor-pointer`
+                }
+                onClick={() => {
+                  setActive(link.title);
+                  setToggle(!toggle);
+                }}
+                >
                   <a href={`#${link.id}`}>{link.title}</a>
                 </li>
-              })
+              )
             }
-          </ul> */}
-
+          </ul>
+          {/* For small devices , the menu options - hamburger menu  */}
+          <div className='sm:hidden flex flex-1 justify-end items-center'>
+            {/* bt default toggle is false , so menu button will be shown , clicking on menu btton(setToggle(!toggle)) will turns it to true which will display close button */}
+            <img 
+            className='h-[28px] w-[28px] object-contain items-center'
+            src={toggle ? close : menu}
+            alt='menu'
+            onClick={() =>setToggle(!toggle)}
+            />
+            {/* inner mobile menu  */}
+            <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl `}>
+            <ul className='list-none flex flex-col justify-end items-start gap-4 '>
+            {
+              navLinks.map((link) => 
+                 <li 
+                  key={link.id}
+                  className={
+                  `${
+                    active === link.title? "text-white" : "text-secondary"
+                  } hover:text-white text-[18px] font-medium cursor-pointer`
+                }
+                onClick={() => setActive(link.title)}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              )
+            }
+          </ul>
+            </div>
+          </div>
      </div>
     </nav>
   )
